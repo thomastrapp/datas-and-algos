766ec306003f37c1d0a5fa6fd6e4d75a5e5a5336
Tom <tom@universe>
2013-07-05 04:20:11 +0200

    Added algorithm sort-and-count-inversions

---
+++ src/al/sort-and-count-inversions.h
@@ -0,0 +1,101 @@
+#ifndef AL_SORT_AND_COUNT_INVERSIONS_H
+#define AL_SORT_AND_COUNT_INVERSIONS_H
+
+#include <cstddef>
+#include <cstring>
+#include <vector>
+#include <iterator>
+
+namespace al {
+
+// Maximum amount of inversions in an array with length n: n * (n - 1) / 2
+// Possible defense (not implemented): 
+//  if( std::numeric_limits<counter>::is_specialized )
+//  {
+//    // If provided type cannot hold amount of possible inversions
+//    if( std::numeric_limits<counter>::max() < size * (size - 1) / 2 )
+//    {
+//      throw std::out_of_range("provide wider counter type");
+//    }
+//  }
+
+template<typename in_iter, typename out_iter, typename counter = unsigned long>
+counter sort_and_count_inversions_merge(
+  in_iter left, 
+  in_iter left_end,
+  in_iter right,
+  in_iter right_end,
+  out_iter out
+)
+{
+  counter inversions = 0;
+  while( left != left_end || right != right_end )
+  {
+    if( left != left_end && right != right_end )
+    {
+      if( *left <= *right )
+      {
+        *out = *left;
+        ++out;
+        ++left;
+      }
+      else
+      {
+        typename std::iterator_traits<in_iter>::difference_type
+          left_to_go = left_end - left;
+        inversions += static_cast<counter>(left_to_go);
+
+        *out = *right;
+        ++out;
+        ++right;
+      }
+    }
+    else if( left != left_end )
+    {
+      *out = *left;
+      ++out;
+      ++left;
+    }
+    else
+    {
+      *out = *right;
+      ++out;
+      ++right;
+    }
+  }
+
+  return inversions;
+}
+
+template<typename iter, typename counter = unsigned long>
+counter sort_and_count_inversions(iter begin, iter end)
+{
+  typedef typename std::iterator_traits<iter>::difference_type iter_range;
+  iter_range size = end - begin;
+
+  if( size < 2 )
+    return 0;
+
+  counter inversions = 0;
+  iter_range center = size / 2;
+  inversions += sort_and_count_inversions(begin, begin + center);
+  inversions += sort_and_count_inversions(begin + center, end);
+
+  std::vector<typename std::iterator_traits<iter>::value_type> v;
+  v.reserve(static_cast<size_t>(size));
+
+  inversions += sort_and_count_inversions_merge(
+    begin, begin + center, 
+    begin + center, end,
+    std::back_inserter(v)
+  );
+
+  std::copy(v.begin(), v.end(), begin);
+
+  return inversions;
+}
+
+
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "../test/src/al/mergesort/random-number-array.h"
 
 #include "al/mergesort.h"
+#include "al/sort-and-count-inversions.h"
 
 //extern std::vector<int> random_numbers;
 
@@ -16,8 +17,8 @@ void print_container(Container& c)
   {
     std::cout << *i << ",";
 
-    if( j % 10 == 0 )
-      std::cout << "\n";
+  //  if( j % 10 == 0 )
+  //    std::cout << "\n";
   }
 
   std::cout << std::endl;
@@ -25,12 +26,18 @@ void print_container(Container& c)
 
 int main()
 {
-  std::vector<int> values {12,1,15,4,15,61,2,3,4,2,1,1};
+//  std::vector<int> v {12,1,15,4,15,61,2,3,4,2,1,1};
+  std::vector<int> v {6, 9, 1, 14, 8, 12, 3, 2};
+//  std::vector<int> v {1,3,5,2,4,6};
 
-  al::mergesort(random_numbers.begin(), random_numbers.end());
-//  std::sort(random_numbers.begin(), random_numbers.end());
+  print_container(v);
+  unsigned long inversions = 0;
+  
+  inversions = al::sort_and_count_inversions(v.begin(), v.end());
 
-  print_container(random_numbers);
+  print_container(v);
+
+  std::cout << "Inversions: " << inversions << std::endl;
 
   return EXIT_SUCCESS;
 }
