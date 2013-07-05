ad09ee86a950b1beaf0aabbed41fd62d755d7e65
Tom <tom@universe>
2013-07-05 17:42:37 +0200

    Read integers from file, sort and count inversions

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,46 @@
 #include <cstdlib>
 #include <iostream>
-
-#include "../test/src/al/mergesort/random-number-array.h"
+#include <fstream>
 
 #include "al/mergesort.h"
 #include "al/sort-and-count-inversions.h"
 
-//extern std::vector<int> random_numbers;
-
-template<typename Container>
-void print_container(Container& c)
-{
-  typename Container::const_iterator i = c.begin();
-  int j = 1;
-  for(; i != c.end(); ++i, ++j)
-  {
-    std::cout << *i << ",";
-
-  //  if( j % 10 == 0 )
-  //    std::cout << "\n";
-  }
-
-  std::cout << std::endl;
-}
-
 int main()
 {
-//  std::vector<int> v {12,1,15,4,15,61,2,3,4,2,1,1};
-  std::vector<int> v {6, 9, 1, 14, 8, 12, 3, 2};
-//  std::vector<int> v {1,3,5,2,4,6};
+  std::vector<int> v;
+  v.reserve(100000);
 
-  print_container(v);
-  unsigned long inversions = 0;
-  
-  inversions = al::sort_and_count_inversions(v.begin(), v.end());
+  {
+    std::ifstream f("data/integers.txt");
 
-  print_container(v);
+    if( !f.is_open() )
+    {
+      std::cout << "cannot open file for reading" << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    int num = 0;
+    while( f.good() )
+    {
+      if( f >> num )
+        v.push_back(num);
+    }
+
+    f.close();
+  }
+
+  std::cout << "Read file, " 
+            << v.size() 
+            << " integers loaded" 
+            << std::endl;
+
+  size_t inversions = 0;
+  inversions = 
+    al::sort_and_count_inversions<
+      typename std::vector<int>::iterator, 
+      size_t
+    >
+  (v.begin(), v.end());
 
   std::cout << "Inversions: " << inversions << std::endl;
 
