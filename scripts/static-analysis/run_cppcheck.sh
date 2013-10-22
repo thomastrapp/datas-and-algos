ee26c0f29a31a8f4e17ef7b9121ab1bcca0accf8
Tom <tom@universe>
2013-10-22 20:29:41 +0200

    run_cppcheck.sh: assume all code is within /src, ignore everything else

--- scripts/static-analysis/run_cppcheck.sh
+++ scripts/static-analysis/run_cppcheck.sh
@@ -17,10 +17,14 @@ assert_dependencies()
   # Try to execute cpp check
   "$cppcheck" --help > /dev/null 2>&1 \
     || { echo >&2 "Cannot execute cppcheck"; exit 1; }
-  
-  # check if first parameter is an existing directory
+ 
+  # check if a directory was passed
   [ -d "$1" ] \
-    || { echo >&2 "Given parameter is not a directory"; exit 1; } 
+    || { echo >&2 "Project root not found"; exit 1; } 
+
+  # check if src directory exists
+  [ -d "$1/src" ] \
+    || { echo >&2 "src directory not found"; exit 1; } 
 }
 
 print_usage()
@@ -29,18 +33,19 @@ print_usage()
   echo "       Wrapper for cppcheck"
 }
 
-# print usage and exit if number of arguments != 1
 [ $# -eq 1 ] || { print_usage ; exit 0; }
 
-# exit if any dependencies are not met
-assert_dependencies "$1"
+assert_dependencies $1
 
-# run cppcheck on all cpp files, enable all checks, use $1 as base directory 
+path="$1/src"
+
+# run cppcheck on all cpp files, enable all checks, use $path as base directory 
 # for includes 
 # xargs echo -n: remove trailing newline from arguments
 # This might break with filenames containing spaces
-"$cppcheck" -I "$1" --quiet --enable=all --error-exitcode=1 \
-  $(find "$1" -name "*.h" -or -name "*.cpp" -or -name "*.hpp" | xargs echo -n)\
+"$cppcheck" -I "$path" --quiet --enable=all --error-exitcode=1 \
+  $(find "$path" -name "*.h" -or -name "*.cpp" -or -name "*.hpp" | \
+    xargs echo -n)\
     2>&1 | sed "s/\[[^]]*\]/${color_highlight}&${color_end}/"
 
 # return with cppcheck's return code 
