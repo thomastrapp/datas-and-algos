d04e00e1fafc03aa1c0b525c999091569467fae4
Tom <tom@universe>
2013-10-24 04:35:50 +0200

    added test cases for priority_queue

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,15 +1,16 @@
 #include "gtest/gtest.h"
 
-#include "ds/stack/main.h"
-#include "ds/square-matrix/main.h"
-#include "ds/binary-search-tree/main.h"
-#include "ds/fixed-hashtable/main.h"
-
 #include "al/mergesort/main.h"
 #include "al/sort-and-count-inversions/main.h"
 #include "al/strassen-matrix-multiply/main.h"
 #include "al/radixsort/main.h"
 #include "al/murmur/main.h"
+
+#include "ds/stack/main.h"
+#include "ds/square-matrix/main.h"
+#include "ds/binary-search-tree/main.h"
+#include "ds/fixed-hashtable/main.h"
+#include "ds/priority-queue/main.h"
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
