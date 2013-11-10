24608979988aef743efcc697ab09d2fecd1352ff
Tom <tom@universe>
2013-11-10 01:58:44 +0100

    consistent formatting

--- src/ds/stack.h
+++ src/ds/stack.h
@@ -9,72 +9,77 @@
 namespace ds {
 
 template<
-  class T, 
-  class C = std::vector<T>
+  class value_type, 
+  class container_type = std::vector<value_type>
 >
 class stack
 {
-  public:
-    stack()
-      : cont()
-    {}
+public:
+  stack()
+  : cont()
+  {
+  }
 
-    stack(const stack<T, C>& other)
-      : cont(other.cont)
-    {
-    }
+  stack(const stack<value_type, container_type>& other)
+  : cont(other.cont)
+  {
+  }
 
-    stack(stack<T, C>&& other)
-      : cont(std::move(other.cont))
-    {
-      other.cont.clear();
-    }
+  stack(stack<value_type, container_type>&& other)
+  : cont(std::move(other.cont))
+  {
+    other.cont.clear();
+  }
 
-    stack<T, C>& operator=(stack<T, C> other)
-    {
-      this->swap(*this, other);
-      return *this;
-    }
+  stack<value_type, container_type>& 
+  operator=(stack<value_type, container_type> other)
+  {
+    this->swap(*this, other);
+    return *this;
+  }
 
-    bool empty() const
-    {
-      return this->cont.empty();
-    }
+  bool empty() const
+  {
+    return this->cont.empty();
+  }
 
-    typename C::size_type size() const
-    {
-      return this->cont.size();
-    }
+  typename container_type::size_type size() const
+  {
+    return this->cont.size();
+  }
 
-    typename C::value_type top() const
-    {
-      if( this->cont.empty() )
-        throw std::out_of_range("cannot top, stack is empty");
+  value_type top() const
+  {
+    if( this->cont.empty() )
+      throw std::out_of_range("cannot top, stack is empty");
 
-      return this->cont.back();
-    }
+    return this->cont.back();
+  }
 
-    void pop()
-    {
-      if( this->cont.empty() )
-        throw std::out_of_range("cannot pop, stack is empty");
+  void pop()
+  {
+    if( this->cont.empty() )
+      throw std::out_of_range("cannot pop, stack is empty");
 
-      this->cont.pop_back();
-    }
+    this->cont.pop_back();
+  }
 
-    void push(const typename C::value_type& elem)
-    {
-      this->cont.push_back(elem);
-    }
+  void push(const value_type& elem)
+  {
+    this->cont.push_back(elem);
+  }
 
-  private:
-    void swap(stack<T, C>& left, stack<T, C>& right)
-    {
-      using std::swap;
-      swap(left.cont, right.cont);
-    }
+private:
+  void swap(
+    stack<value_type, container_type>& left, 
+    stack<value_type, container_type>& right
+  )
+  {
+    using std::swap;
+    swap(left.cont, right.cont);
+  }
 
-    C cont;
+  container_type cont;
 };
 
 
