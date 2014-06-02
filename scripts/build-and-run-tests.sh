ba5109a5a7b96e98ec3c397b8c8be31db1b39eb7
Tom <tom@universe>
2014-06-02 16:07:36 +0200

    changed test executable name to project name

--- scripts/build-and-run-tests.sh
+++ scripts/build-and-run-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd test/build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make && ./data-structures-test
+cd test/build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make && ./datas-and-algos-test
 
 exit $?
 
